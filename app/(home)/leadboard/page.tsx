0faad68dfb26a9d49e5467664c6e9ac7dde18352
Uncle Moses <kiskayemoses@gmail.com>
2024-04-30 13:13:23 +0300

    feat: initial commit 🎉

--- app/(home)/leadboard/page.tsx
+++ app/(home)/leadboard/page.tsx
@@ -1,6 +1,13 @@
 import { getUsers } from "@/actions/users";
 import LeadBoard from "@/components/leadboard";
 import { getCurrentUser } from "@/lib/authProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lead board",
+  description:
+    "Daily Front-end Development short resources and tricks for React, TypeScript HTML, CSS, and Vanilla JavaScript, alongside essential libraries and tools. Build 10X faster with the best resources",
+};
 
 export default async function Component() {
   const users = await getUsers();
